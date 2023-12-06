599c9b8f179c246b6e39773b7afdeaa977c0bb84
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2023-12-06 16:35:01 +0500

    test api

--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ import { TodoRouter, CompleteRouter, AddRouter } from "./Routes/Routes.js";
 
 mongoose.connect(process.env.MONGO_URL);
 
-const app = express();
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.listen(3000, () => console.log("Server running on port 3000!"));
+    const app = express();
+    app.use(bodyParser.json({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
+    app.listen(3000, () => console.log("Server running on port 3000!"));
 
 
+app.use('/',{req,res}=>res.json("heyy"));
 app.use('/get', TodoRouter);
 app.use('/add', AddRouter);
 app.use('/complete', CompleteRouter);
